f5e59f45a87d59926ad524fb0f572582bc6484b5
INVOKER <developer.invoker@gmail.com>
2023-08-01 13:16:22 +0430

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      fontFamily: {
+        sans: ["vazir"],
+      },
+    },
+  },
+  plugins: [],
+};
