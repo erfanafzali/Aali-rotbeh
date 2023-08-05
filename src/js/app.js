5627b49de26fad18e736d060f8d5a93dcb70740e
INVOKER <developer.invoker@gmail.com>
2023-08-05 18:20:35 +0430

    updated btn

---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+const containerBtn = document.querySelector(".container-btn")
+const allBtn = document.querySelectorAll(".btn")
+const hiddenInput = document.querySelector(".input-hidden")
+const closeBtn = document.querySelector(".btns")
+const backDrop = document.querySelector(".modal")
+
+
+containerBtn.addEventListener("click", getBtnValue)
+
+
+function getBtnValue(e) {
+    const getBtnName = e.target
+    // getBtnName.innerText = ""
+
+    allBtn.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            console.log(e.target.value);
+
+            getBtnName.innerText = e.target.value
+            closeBtn.style.display = "none"
+            backDrop.style.display = "none"
+        })
+    })
+}
+
